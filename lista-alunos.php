f70b687f6140a459875e61496ea59e21ee5c9fcf
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 01:35:30 -0300

    :tv: add aula 4

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,9 +1,23 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=ead_php_alura_pdo;user=postgres;password=123456");
 
 $statement = $pdo->query('SELECT * FROM students;');
-var_dump($statement->fetchAll());
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$studentList = [];
+
+foreach ($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentList);
