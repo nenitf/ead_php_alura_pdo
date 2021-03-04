7380025d6e57b8a57ccacea8f7a3db5b35d75c48
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 01:51:57 -0300

    :tv: add aula 6

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -9,8 +9,17 @@ $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=ead_php_alura_pdo;user=pos
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$student = new Student(null, 'Vinicius Dias', new \DateTimeImmutable('1997-10-15'));
+$student = new Student(
+    null,
+    "Patrícia Freitas",
+    new DateTimeImmutable('1986-10-25')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()) {
+    echo "Aluno incluído";
+}
