42e2307da9d73969d5ebb5346a205c08281840c9
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 01:40:14 -0300

    :tv: add aula 5

--- lista-alunos.php
+++ lista-alunos.php
@@ -12,6 +12,17 @@ $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $studentList = [];
 
+//while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+//    $student = new Student(
+//        $studentData['id'],
+//        $studentData['name'],
+//        new \DateTimeImmutable($studentData['birth_date'])
+//    );
+//
+//    echo $student->age() . PHP_EOL;
+//}
+//exit();
+
 foreach ($studentDataList as $studentData) {
     $studentList[] = new Student(
         $studentData['id'],
