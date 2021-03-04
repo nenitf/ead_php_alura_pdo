6c50460597e3ab160078d0a3593fc844c21b92dd
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 14:59:57 -0300

    :tv: add aula 13

--- src/Infrastructure/Repository/PdoStudentRepository.php
+++ src/Infrastructure/Repository/PdoStudentRepository.php
@@ -39,6 +39,10 @@ class PdoStudentRepository implements StudentRepository
         $insertQuery = 'INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);';
         $stmt = $this->connection->prepare($insertQuery);
 
+        if ($stmt === false) {
+            throw new \RuntimeException('Erro na query do banco');
+        }
+
         $success = $stmt->execute([
             ':name' => $student->name(),
             ':birth_date' => $student->birthDate()->format('Y-m-d'),
